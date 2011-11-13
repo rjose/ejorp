8e90c85d46620081d670abafad6b988820c36234
Rino Jose <rjose@ejorp.com>
2011-11-12 17:05:32 -0800

    Added initial test file

---
+++ war/src/test/java/com/ejorp/core/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com.ejorp.core;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rjose
+ * Date: 11/12/11
+ * Time: 10:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldTest {
+    @Test
+    public void testGetHtml() throws Exception {
+        assert 1 == 1;
+    }
+}
