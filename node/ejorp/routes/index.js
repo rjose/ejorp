c3c0b95ef3fe88ead464c564f9fb88721b8afc1e
Rino Jose <rjose@ejorp.com>
2011-11-16 22:44:24 -0800

    Demonstration that we can access couch data from node

--- node/ejorp/routes/index.js
+++ node/ejorp/routes/index.js
@@ -1,7 +1,33 @@
 /*
  * GET home page.
  */
+var http = require('http');
+
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  var options = {
+    host: '127.0.0.1',
+    port: 5984,
+    path: '/hello-world/123',
+    method: 'GET'
+  };
+
+  var request = http.request(options, function(myres) {
+    var body = '';
+    console.log('STATUS: ' + myres.statusCode);
+    myres.setEncoding('utf8');
+    
+    myres.on('data', function(chunk) {
+      console.log('BODY: ' + chunk);
+      body = body + chunk;
+    })
+  
+    myres.on('end', function() {
+      res.render('index', { title: 'Express', body: body })
+    });
+  });
+
+  request.end();
+
+  //http://127.0.0.1:5984/hello-world/123
 };
