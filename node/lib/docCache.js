997e1c931773f27ae5095ca101dc0cb1000da196
Rino Jose <rjose@ejorp.com>
2011-11-24 19:24:59 -0800

    Can read document out of redis

--- node/lib/docCache.js
+++ node/lib/docCache.js
@@ -1,13 +1,26 @@
+var Redis = require("redis");
+
+m_client = Redis.createClient();
+// TODO: Handle redis errors
+
 function getDocument(documentId, callback) {
-  // TODO: Make the real call or read this info from file
-  var document = {
-    tasks: [
-      {title: 'Get server up', recentActivity: [{personId: 10, picture: 'http://image.png'}], isActive: true},
-      {title: 'Get server deployed', recentActivity: [{personId: 14, picture: 'http://image.png'}], isActive: false},
-      {title: 'Test server', recentActivity: [{personId: 30, picture: 'http://image.png'}], isActive: false}],
-    alert: {type: 'FIRST_TIME_USER'}};
+  // TODO: Figure out how to stub this out
+  m_client.get(documentId, function(err, docString) {
+    if(err) {
+      // TODO: Handle error
+    }
+    callback(null, JSON.parse(docString));
+  });
+
+  //var document = {
+    //tasks: [
+      //{title: 'Get server up', recentActivity: [{personId: 10, picture: 'http://image.png'}], isActive: true},
+      //{title: 'Get server deployed', recentActivity: [{personId: 14, picture: 'http://image.png'}], isActive: false},
+      //{title: 'Test server', recentActivity: [{personId: 30, picture: 'http://image.png'}], isActive: false}],
+    //alert: {type: 'FIRST_TIME_USER'}};
+  //m_client.set(documentId, JSON.stringify(document));
   
-  callback(null, document);
+  //callback(null, document);
   return;
 }
 
