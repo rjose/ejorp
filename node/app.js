29466fe10a1aa897873017b3a43944eebfad9669
Rino Jose <rjose@ejorp.com>
2011-11-27 22:59:50 -0800

    Removed socket examples

--- node/app.js
+++ node/app.js
@@ -23,22 +23,8 @@ app.configure('production', function(){
 });
 
 // Routes
-// TODO: Delete all of the socket examples
-//app.get('/', routes.index);
-//app.get('/socket-example', function(req, res) {
-  //res.render('socket-example.ejs', {layout: false});
-//});
 RootController.addRoutes(app);
 TasksController.addRoutes(app, '/');
-
-
-//io.sockets.on('connection', function(socket) {
-  //socket.emit('news', {hello: 'world'});
-  //socket.on('my other event', function(data) {
-    //console.log("----->")
-    //console.log(data);
-  //});
-//});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
