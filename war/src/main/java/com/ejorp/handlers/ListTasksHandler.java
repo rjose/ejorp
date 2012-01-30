cb93ae2fec37064671088a9118203d41c5b9c1d6
James Simonsen <simonjam@chromium.org>
2012-01-29 23:36:34 -0800

    Add skeleton handler for list_tasks.

---
+++ war/src/main/java/com/ejorp/handlers/ListTasksHandler.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ejorp.core;
+
+import com.google.gson.Gson;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * REST Web Service
+ *
+ * @author simonjam
+ */
+@Path("list_tasks")
+public class ListTasksHandler {
+    private Logger logger = Logger.getLogger("ejorp-spike");
+
+    @Context
+    private UriInfo context;
+
+    /** Creates a new instance of ListTasksHandler*/
+    public ListTasksHandler() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.ejorp.helloworld.HelloWorld
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/json")
+    public String getHtml() throws IOException {
+        Gson gson = new Gson();
+        double[] coeffs = {0.0, 10.0};
+        return gson.toJson(coeffs);
+    }
+}
